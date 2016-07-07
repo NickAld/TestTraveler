23485aeb896f10ad527bd0ce1c9661e39e1e5ad4
Aldoshin <rus-prapor@yandex.ru>
2016-07-07 17:35:21 +0300

    Изменение проекта
    Во время перезаливки выложил только часть проекта.
    Проект разбит на две части

---
+++ Sorted/Card.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestTravelerSorted
+{
+    public class Card
+    {
+        public Card(string value)
+        {
+            string[] points = value.Split(';');
+            pointFrom = points[0];
+            pointTo = points[1];
+        }
+
+        public string pointFrom { get; set; }
+        public string pointTo { get; set; }
+        
+    }
+    
+}
---
+++ Sorted/Sorted.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestTravelerSorted
+
+{
+    public class Sorted
+    {
+        private Dictionary<string, Card> cardsInFrom;
+        private Dictionary<string, Card> cardsInTo;
+        private List<Card> cardsOut = new List<Card>();
+
+        public List<Card> CardSorted
+        {
+            get { return cardsOut; }
+        }
+        public Sorted(Dictionary<string,Card> _cardsFrom, Dictionary<string, Card> _cardsTo)
+        {
+            cardsInFrom = _cardsFrom;
+            cardsInTo = _cardsTo;
+        }
+
+        public void Sort()
+        {
+            Card cardStart=null;
+            
+            /// Определяем стартовую позицию
+            foreach (var c in cardsInFrom.Values)
+                if (!cardsInTo.Keys.Contains(c.pointFrom))
+                    cardStart = c;
+
+
+
+            if (cardStart != null)
+                cardsOut.Add(cardStart);
+            else
+                //Стартовая позиция не найдена, список замкнут
+                cardStart = cardsInFrom.Values.First();
+
+            foreach (var c in cardsInFrom)
+            {
+
+                if (cardsInFrom.Keys.Contains(cardStart.pointTo))
+                {
+                    cardsOut.Add(cardsInFrom[cardStart.pointTo]);
+                    cardStart = cardsInFrom[cardStart.pointTo];
+                }
+            }
+        }
+
+    }
+}
